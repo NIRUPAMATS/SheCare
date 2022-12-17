7b8a302f5a525a692dc8bc08c80fe400e65d5711
Aiswarya-MS <aiswaryamsaisw@gmail.com>
2022-12-17 15:08:36 +0530

    third commit

---
+++ lib/calendar.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:ammuna/home.dart';
+class Calendar extends StatefulWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  CalendarFormat format=CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay  = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar : AppBar(
+        backgroundColor: Color(0xff3d3a3a),
+        title : Center(
+          child: Text('Calender',
+            style: TextStyle(
+              letterSpacing: 2.0,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Home()));
+            },
+          ),
+        ],
+      ),
+
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xffff385d),
+              Colors.white,
+            ],
+          ),
+        ),
+        height: MediaQuery.of(context).size.height,
+        child:Column(
+          children: [
+            TableCalendar(
+
+              focusedDay: selectedDay,
+              firstDay:DateTime(1998),
+              lastDay: DateTime(2050),
+              calendarFormat: format,
+              onFormatChanged: (CalendarFormat _format){
+                setState(() {
+                  format= _format;
+                });
+              },
+              startingDayOfWeek: StartingDayOfWeek.sunday,
+              daysOfWeekVisible: true,
+
+              //Day changed
+              onDaySelected: (DateTime selectDay,DateTime focusDay)
+              {
+                setState(() {
+                  selectedDay=selectDay;
+                  focusedDay=focusDay;
+                });
+                print(focusedDay);
+              },
+              selectedDayPredicate: (DateTime date){
+                return isSameDay(selectedDay, date);
+              },
+
+
+              //To style the calendar
+              calendarStyle: CalendarStyle(isTodayHighlighted: true,
+                  selectedDecoration: BoxDecoration(
+                    color:Colors.black38,
+                    shape: BoxShape.circle,),
+                  selectedTextStyle: TextStyle(color: Colors.white),
+                  todayDecoration: BoxDecoration(
+                    color:Color(0xffff5e6b),
+                    shape: BoxShape.circle,
+                  )
+              ),
+
+              headerStyle: HeaderStyle(
+                formatButtonVisible: true,
+                titleCentered: true,
+                formatButtonShowsNext: false,
+                formatButtonDecoration: BoxDecoration(
+                  color: Color(0xffff385d),
+                  borderRadius: BorderRadius.circular(5.0)
+                  ,          ),
+                formatButtonTextStyle: TextStyle(color:Colors.white,backgroundColor: Color(0xffff5e6b)),
+              ),
+            ),
+            SizedBox(
+              height: 10 ,
+            ),
+
+          ],
+        ),
+
+      ),
+
+
+    );
+
+
+
+
+
+  }
+
+}
---
+++ lib/journal.dart
@@ -0,0 +1,139 @@
+import 'package:ammuna/db/model/data_model_diary.dart';
+import 'package:ammuna/diary.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:ammuna/db/functions/db_functions_diary.dart';
+import 'package:ammuna/db/model/data_model_diary.dart';
+import 'package:intl/intl.dart';
+import 'package:ammuna/home.dart';
+
+
+
+String title = "";
+String data = "";
+final _namecontroller = TextEditingController();
+final _datacontroller = TextEditingController();
+
+class Journal extends StatefulWidget {
+  const Journal({Key? key}) : super(key: key);
+
+  @override
+  State<Journal> createState() => _JournalState();
+
+}
+
+class _JournalState extends State<Journal> {
+  //bool? value = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar : AppBar(
+        backgroundColor: Color(0xff3d3a3a),
+        title : Center(
+          child: Text('My Journal',
+            style: TextStyle(
+              letterSpacing: 2.0,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Home()));
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(child:ValueListenableBuilder(
+        valueListenable: filelistnotifier,
+        builder:(BuildContext context, List<FileData_diary>filelist, Widget? child) {
+          return Column(
+            children: [
+
+              Container(
+                height: 100,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xffff385d),
+                      Color(0xffff385d),
+                    ],
+                  ),
+                ),
+                child: Center(
+                  child: Text("Hey there\nWhat's on your mind?!",
+                    style: TextStyle(
+                      fontFamily: 'Heland',
+                      color: Colors.white,
+                      fontSize: 25.0,
+                      letterSpacing: 2.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height:15.0),
+              Expanded(child:
+              ListView.separated(itemBuilder: (BuildContext context,int index){
+                return Container(
+                  height: 100,
+                  child: ListTile(
+                    title:Text(filelist[index].title),
+                    leading: Text(filelist[index].dateTime),
+                    subtitle: Container(
+                        child: Text(filelist[index].data)
+                    ),
+                    trailing:
+                    Wrap(
+                      children: [
+                        IconButton(onPressed: (){
+                          setState(() {
+                            if(filelist[index].id!=null){
+                              deleteFiles(filelist[index].id!);
+                            }
+                          });
+                        }, icon: Icon(Icons.edit,color:Colors.black54)),
+                        IconButton(onPressed: (){
+                          setState(() {
+                            if(filelist[index].id!=null){
+                              deleteFiles(filelist[index].id!);
+                            }
+                          });
+                        }, icon: Icon(Icons.delete,color:Colors.black54))
+                      ],
+                    ),
+
+                  ),
+                );
+              },
+                  separatorBuilder: (BuildContext context, int index) => const Divider(),
+                  itemCount: filelist.length),),
+
+            ],
+          );
+        },
+      )
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Diary()));
+        },
+        tooltip: 'New File',
+        backgroundColor: Colors.pinkAccent[200],
+        child: Icon(Icons.add),
+      ),
+
+
+    );
+
+  }
+
+}
