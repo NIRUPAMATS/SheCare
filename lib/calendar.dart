092cec2cebedd1a6a2b71ec00e14eb0facf7b514
Nirupama T S <nirupamasajeevan@gmail.com>
2022-12-18 02:55:49 +0530

    aa

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:shecare/home.dart';
+
+
 class Calendar extends StatefulWidget {
   const Calendar({Key? key}) : super(key: key);
 
@@ -49,60 +51,91 @@ class _CalendarState extends State<Calendar> {
         height: MediaQuery.of(context).size.height,
         child:Column(
           children: [
-            TableCalendar(
+            SizedBox(height: 5,),
+            Card(
+              color: Colors.white,
+              child: TableCalendar(
+                headerVisible: true,
+                focusedDay: selectedDay,
+                firstDay:DateTime(1998),
+                lastDay: DateTime(2050),
+                calendarFormat: format,
+                onFormatChanged: (CalendarFormat _format){
+                  setState(() {
+                    format= _format;
+                  });
+                },
+                startingDayOfWeek: StartingDayOfWeek.sunday,
+                daysOfWeekVisible: true,
 
-              focusedDay: selectedDay,
-              firstDay:DateTime(1998),
-              lastDay: DateTime(2050),
-              calendarFormat: format,
-              onFormatChanged: (CalendarFormat _format){
-                setState(() {
-                  format= _format;
-                });
-              },
-              startingDayOfWeek: StartingDayOfWeek.sunday,
-              daysOfWeekVisible: true,
+                //Day changed
+                onDaySelected: (DateTime selectDay,DateTime focusDay)
+                {
+                  setState(() {
+                    selectedDay=selectDay;
+                    focusedDay=focusDay;
+                  });
+                  print(focusedDay);
+                },
+                selectedDayPredicate: (DateTime date){
+                  return
 
-              //Day changed
-              onDaySelected: (DateTime selectDay,DateTime focusDay)
-              {
-                setState(() {
-                  selectedDay=selectDay;
-                  focusedDay=focusDay;
-                });
-                print(focusedDay);
-              },
-              selectedDayPredicate: (DateTime date){
-                return isSameDay(selectedDay, date);
-              },
+                    isSameDay(selectedDay, date);
+
+                },
 
 
-              //To style the calendar
-              calendarStyle: CalendarStyle(isTodayHighlighted: true,
-                  selectedDecoration: BoxDecoration(
-                    color:Colors.black38,
-                    shape: BoxShape.circle,),
-                  selectedTextStyle: TextStyle(color: Colors.white),
-                  todayDecoration: BoxDecoration(
-                    color:Color(0xffff5e6b),
-                    shape: BoxShape.circle,
-                  )
-              ),
+                //To style the calendar
+                calendarStyle: CalendarStyle(
+                    isTodayHighlighted: true,
+                    selectedDecoration: BoxDecoration(
+                      color:Colors.black38,
+                      shape: BoxShape.circle,),
+                    selectedTextStyle: TextStyle(color: Colors.white),
+                    todayDecoration: BoxDecoration(
+                      color:Color(0xffff5e6b),
+                      shape: BoxShape.circle,
+                    )
+                ),
 
-              headerStyle: HeaderStyle(
-                formatButtonVisible: true,
-                titleCentered: true,
-                formatButtonShowsNext: false,
-                formatButtonDecoration: BoxDecoration(
-                  color: Color(0xffff385d),
-                  borderRadius: BorderRadius.circular(5.0)
-                  ,          ),
-                formatButtonTextStyle: TextStyle(color:Colors.white,backgroundColor: Color(0xffff5e6b)),
+                headerStyle: HeaderStyle(
+                  formatButtonVisible: true,
+                  titleCentered: true,
+                  formatButtonShowsNext: false,
+                  formatButtonDecoration: BoxDecoration(
+                    color: Color(0xffff385d),
+                    borderRadius: BorderRadius.circular(5.0)
+                    ,          ),
+                  formatButtonTextStyle: TextStyle(color:Colors.white,),
+                ),
               ),
             ),
             SizedBox(
-              height: 10 ,
+              height: 40 ,
             ),
+            Container(
+              height: MediaQuery.of(context).size.height/3.5,
+              width: MediaQuery.of(context).size.width-40,
+              child: Card(
+                color: Colors.white54,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: Center(
+                  child:
+                      Text(
+                        'High chance of getting period!',
+                        style: TextStyle(
+                          color: Color(0xffff004e),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25
+                        ),
+                      ),
+
+                  ),
+                ),
+              ),
+
 
           ],
         ),
