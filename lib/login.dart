d192eade73df065ce4218d2d4f0c2b6a955e6647
SnehaSani <snehasanip@gmail.com>
2022-12-17 15:14:59 +0530

    4th commit

---
+++ lib/login.dart
@@ -0,0 +1,130 @@
+import 'package:ammuna/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:ammuna/db/functions/login_functions.dart';
+
+import 'home.dart';
+
+
+final mailinputcontroller=TextEditingController();
+final passwordinputcontroller=TextEditingController();
+final namecontroller=TextEditingController();
+
+String _email="",_password='',_repassword='';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+final mailcontroller = TextEditingController();
+final passwordcontroller = TextEditingController();
+
+String lemail='',lpassword='';
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffff5e6b),
+      body: Padding(padding:
+      const EdgeInsets.all(25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+               child:CircleAvatar(
+                  backgroundImage:  AssetImage('assets/emblem.jpeg'),
+                 radius: 60.0,
+            ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top:10.0,bottom: 70.0),
+              child: Text('Log In',
+                style:TextStyle(
+                  fontSize:35.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),),
+            ),
+
+            TextFormField(
+              controller: mailinputcontroller,
+              decoration: const InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: InputBorder.none,
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                      borderSide: BorderSide(
+                        color:Colors.white,
+                      )
+                  )
+              ),
+            ),
+
+            const SizedBox(height:10),
+            TextFormField(
+              controller: passwordinputcontroller,
+              decoration: const InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: InputBorder.none,
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                      borderSide: BorderSide(
+                        color:Colors.white,
+                      )
+                  )
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height/50,),
+            ElevatedButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.white)),
+                onPressed: () async {
+                lemail=mailcontroller.text;
+                lpassword=passwordcontroller.text;
+                if(await checkPassword(lemail, lpassword)==0){
+                  showDialog(context: context, builder: (context){
+                    return const Dialog(child:Text('Invalid credentials',style: TextStyle(color: Colors.red),),
+                  );
+                }
+              );
+              mailcontroller.clear();
+              passwordcontroller.clear();
+              }
+              else{
+                loadPage(lemail, context);
+              }
+              },
+                child: Text(
+                  'Login',
+                  style:TextStyle(
+                  color: Colors.pinkAccent ,
+                  fontSize: 15.0,
+                  letterSpacing: 1.0,
+                  ),
+                ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                const Text("Don't have an account?"),
+                TextButton(onPressed: (){
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (ctx)=>SignIn())
+                  );
+                },
+                    child: const Text("Sign In",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/signup.dart
@@ -0,0 +1,299 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:ammuna/account.dart';
+import 'package:ammuna/home.dart';
+import 'package:ammuna/db/functions/login_functions.dart';
+import 'package:ammuna/db/model/data_model_account.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+final mailinputcontroller=TextEditingController();
+final passwordinputcontroller=TextEditingController();
+final repasswordinputcontroller=TextEditingController();
+final namecontroller=TextEditingController();
+String _email="",_password='',_repassword='';
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 245, 240, 198),
+        body: Padding(padding:
+        const EdgeInsets.all(20),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: mailinputcontroller,
+                  decoration: const InputDecoration(
+                      hintText: 'email',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                          borderSide: BorderSide())),
+                ),
+                const SizedBox(height: 30),
+                TextField(
+                  obscureText: true,
+                  controller: passwordinputcontroller,
+                  decoration: const InputDecoration(
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                          borderSide: BorderSide())),
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  obscureText: true,
+                  controller: repasswordinputcontroller,
+                  decoration: const InputDecoration(
+                      hintText: 'Re Enter Password',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                          borderSide: BorderSide())),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                    onPressed: () async {
+                      _email=mailinputcontroller.text;
+                      _password=passwordinputcontroller.text;
+                      _repassword=repasswordinputcontroller.text;
+                      if(_password!=_repassword){
+                        showDialog(context: context, builder: (context){
+                          return  Dialog(
+                              child: Container(child: Text('reEnter the password correcty!!',style: TextStyle(color: Colors.red)),height: 45,)
+                          );
+                        });
+                        repasswordinputcontroller.clear();
+                      }
+                      else{
+                        if(await searchAccount(_email)==1){
+                          showDialog(context: context, builder: (context){
+                            return  Dialog(
+                                child: Container(child: Text('Email already signed in!!',style: TextStyle(color: Colors.red),),height: 45,)
+                            );
+                          });
+                          mailinputcontroller.clear();
+                          repasswordinputcontroller.clear();
+                          passwordinputcontroller.clear();
+                        }
+                        else{
+                          final newacc=Account(email: _email, password: _password);
+                          addAccount(newacc);
+                          showDialog(context: context, builder: (context){
+                            return  Dialog(
+                                child:
+                                Center(
+                                  child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children:[
+                                        const Text('Successfully Signed in!!',style: TextStyle(color: Colors.red)),
+                                        const SizedBox(height: 30),
+                                        Padding(padding: const EdgeInsets.all(30),
+                                          child: TextField(
+                                            controller: namecontroller,
+                                            decoration:const InputDecoration(hintText: 'Enter Name',
+                                                border: OutlineInputBorder(
+                                                    borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                                                    borderSide: BorderSide())
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(height: 30,),
+                                        ElevatedButton(
+                                            onPressed:() async {
+                                              await addDetails(_email, 1, namecontroller.text);
+                                              loadPage(_email, context);
+                                            }, child: const Text('OK'))
+                                      ]
+                                  ) ,
+                                )
+                            );
+                          });
+                        }
+                      }
+                    },
+                    child:const Text(
+                      "Sign In",
+                    )),
+              ]
+          ),
+        ));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:ammuna/db/functions/login_functions.dart';
+// import 'package:ammuna/home.dart';
+// import 'package:ammuna/db/model/data_model_account.dart';
+//
+//
+//
+// class Signup extends StatefulWidget {
+//   const Signup({Key? key}) : super(key: key);
+//
+//   @override
+//   State<Signup> createState() => _SignupState();
+// }
+//
+// final mailinputcontroller=TextEditingController();
+// final passwordinputcontroller=TextEditingController();
+// final repasswordinputcontroller=TextEditingController();
+// final namecontroller=TextEditingController();
+// String email="",_password='',_repassword='';
+//
+//
+// class _SignupState extends State<Signup> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xffff5e6b),
+//             body: Padding(padding:
+//             const EdgeInsets.all(20),
+//                child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Container(
+//                       margin: EdgeInsets.only(bottom: 70.0),
+//                       child: Text('Sign Up',
+//                       style:TextStyle(
+//                         fontSize:35.0,
+//                         fontWeight: FontWeight.bold,
+//                         color: Colors.white,
+//                       ),),
+//                     ),
+//
+//                     TextFormField(
+//                          controller: mailinputcontroller,
+//                            decoration: const InputDecoration(
+//                                hintText: 'Email',
+//                                hintStyle: TextStyle(color: Colors.white),
+//                                border: InputBorder.none,
+//                                enabledBorder: OutlineInputBorder(
+//                                    borderRadius: BorderRadius.all(Radius.circular(4.0)),
+//                                    borderSide: BorderSide(
+//                                      color:Colors.white,
+//                                    )
+//                                )
+//                            ),
+//                     ),
+//
+//                     const SizedBox(height:10),
+//                     TextFormField(
+//                       controller: passwordinputcontroller,
+//                       decoration: const InputDecoration(
+//                         hintText: 'Password',
+//                           hintStyle: TextStyle(color: Colors.white),
+//                         border: InputBorder.none,
+//                         enabledBorder: OutlineInputBorder(
+//                           borderRadius: BorderRadius.all(Radius.circular(4.0)),
+//                           borderSide: BorderSide(
+//                             color:Colors.white,
+//                           )
+//                         )
+//                       ),
+//                     ),
+//
+//                     const SizedBox(height:10),
+//                     TextFormField(
+//                       controller: repasswordinputcontroller,
+//                       decoration: const InputDecoration(
+//                         hintText: 'Re enter Password',
+//                           hintStyle: TextStyle(color: Colors.white),
+//                         border: InputBorder.none,
+//                         enabledBorder: OutlineInputBorder(
+//                             borderRadius: BorderRadius.all(Radius.circular(4.0)),
+//                             borderSide: BorderSide(
+//                               color:Colors.white,
+//                             )
+//                         )
+//                     ),
+//                     ),
+//                     const SizedBox(height: 30),
+//                     ElevatedButton(
+//                         onPressed: () async {
+//                           email=mailinputcontroller.text;
+//                           _password=passwordinputcontroller.text;
+//                           _repassword=repasswordinputcontroller.text;
+//                           if(_password!=_repassword){
+//                             showDialog(context: context, builder: (context){
+//                               return  Dialog(
+//                                   child: Container(child: Text('reEnter the password correcty!!',style: TextStyle(color: Colors.red)),height: 45,)
+//                               );
+//                             });
+//                             repasswordinputcontroller.clear();
+//                           }
+//                           else{
+//                             if(await searchAccount(email)==1){
+//                               showDialog(context: context, builder: (context){
+//                                 return  Dialog(
+//                                     child: Container(child: Text('Email already signed in!!',style: TextStyle(color: Colors.red),),height: 45,)
+//                                 );
+//                               });
+//                               mailinputcontroller.clear();
+//                               repasswordinputcontroller.clear();
+//                               passwordinputcontroller.clear();
+//                             }
+//                             else{
+//                               final newacc=Account(email: email, password: _password);
+//                               addAccount(newacc);
+//                               showDialog(context: context, builder: (context){
+//                                 return  Dialog(
+//                                     child:
+//                                     Center(
+//                                       child: Column(
+//                                           mainAxisAlignment: MainAxisAlignment.center,
+//                                           children:[
+//                                             const Text('Successfully Signed in!!',style: TextStyle(color: Colors.red)),
+//                                             const SizedBox(height: 30),
+//                                             Padding(padding: const EdgeInsets.all(30),
+//                                               child: TextField(
+//                                                 controller: namecontroller,
+//                                                 decoration:const InputDecoration(hintText: 'Enter Name',
+//                                                     border: OutlineInputBorder(
+//                                                         borderRadius: BorderRadius.all(Radius.circular(4.0)),
+//                                                         borderSide: BorderSide())
+//                                                 ),
+//                                               ),
+//                                             ),
+//                                             const SizedBox(height: 30,),
+//                                             ElevatedButton(
+//                                                 onPressed:() async {
+//                                                   await addDetails(email, 1, namecontroller.text);
+//                                                   Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Home()));
+//                                                 },
+//                                                 child: const Text('OK'))
+//                                           ]
+//                                       ) ,
+//                                     )
+//                                 );
+//                               });
+//                             }
+//                           }
+//                         },
+//                         child:const Text(
+//                           "Sign In",
+//                         )
+//                     ),
+//             ],
+//             ),
+//             ),
+//     );
+//   }
+// }
